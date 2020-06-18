cefc29b224a2fc5b1b6ccec1c9dc3e6537751aae
_hoo! <31012876+HandierChan@users.noreply.github.com>
2020-06-18 23:52:48 +0800

    Update mayatoABC.py

--- mayatoABC.py
+++ mayatoABC.py
@@ -1,60 +1,93 @@
+# python3.7
 import os
 from tkinter import *
 
-def MayaMeltoCMD():
+
+def exportAllABC():
+    '''
+    用mel写的maya export ABC，通过python发到cmd执行
+    '''
+
+    # 获取GUI界面参数
     mayaEXE = mayaInstPath.get()+'/bin/mayabatch.exe'
-    mayaPath = mayaPathEntry.get()
+    mayaAbsEXE = correctWinPath(mayaEXE)
+
     abcPath = abcPathEntry.get()
+    abcAbsPath = correctWinPath(abcPath)
 
-    mayaAbsEXE = os.path.abspath(mayaEXE)
-    mayaAbsPath = os.path.abspath(mayaPath)
-    abcAbsPath = os.path.abspath(abcPath)
+    # mayaPath是目录还是maya文件
+    mayaPathJudge = mayaPathEntry.get()
+    if os.path.isfile(mayaPathJudge):
+        mayaPath = os.path.dirname(mayaPathJudge)
+        mayaAbsPath = correctWinPath(mayaPath)
+        mayaFiles = [os.path.basename(mayaPathJudge)]
+    else:
+        mayaAbsPath = correctWinPath(mayaPathJudge)
+        mayaFiles = filterFileExt(mayaAbsPath, ['.ma','.mb'])
 
-    fileLists = [i for i in os.listdir(mayaAbsPath) if os.path.isfile(mayaAbsPath+'/'+i)]
-    mayaExt = ['.ma','.mb']
-    mayaFiles = [i for i in fileLists if os.path.splitext(i)[1] in mayaExt]
 
+    # 循环每个maya文件发到cmd执行
     for i in mayaFiles:
         mayaName = os.path.splitext(i)[0]
-        mayaExt = os.path.splitext(i)[1]
-        mayaExec = '\"'+mayaAbsEXE+'\"'+' -file '+'\"'+mayaAbsPath+'/'+mayaName+mayaExt+'\"'
+        fileExt = os.path.splitext(i)[1]
+        mayaExec = mayaAbsEXE+' -file '+mayaAbsPath+'/'+'"'+mayaName+fileExt+'"'
         mel = 'int $startFrame=`playbackOptions -q -ast`;\
             int $endFrame=`playbackOptions -q -aet`;\
-            string $commandMel=\\"-frameRange \\"+$startFrame+\\" \\"+$endFrame+\\" -file '+'\\\\\\\"'+abcAbsPath+'/'+mayaName+'.abc'+'\\\\\\\"'+' -uvWrite -worldSpace -writeVisibility -dataFormat ogawa\\";\
+            string $commandMel=\\"-frameRange \\"+$startFrame+\\" \\"+$endFrame+\\" -file '+abcAbsPath+'/'+'"'+mayaName+'.abc'+'"'+' -uvWrite -worldSpace -writeVisibility -dataFormat ogawa\\";\
             AbcExport -j $commandMel;'
         cmd = mayaExec+' -command '+'"'+mel+'"'
         os.system(cmd)
-        print(mayaExec)
+        #print(mayaFiles)
 
-def aa():
-    mayaInst=mayaInstPath.get()+'/bin/mayabatch.exe'
-    print(mayaInst)
+def correctWinPath(path):
+    '''
+    纠正路径错误：1反斜杠改成正斜杠；2带空格的目录加上双引号
+    '''
+    absPath = os.path.abspath(path)
+    splitPath = absPath.split('\\')
+    for i in range( len(splitPath)):
+        if ' ' in splitPath[i]:
+            splitPath[i] = '"' + splitPath[i] + '"'
+    windowsPath = '/'.join(splitPath)
+    return windowsPath
+
+def filterFileExt(mayaAbsPath, fileExt):
+    # 过滤目录是否有对应格式的文件
+    fileLists = [i for i in os.listdir(mayaAbsPath) if os.path.isfile(mayaAbsPath+'/'+i)]
+    Files = [i for i in fileLists if os.path.splitext(i)[1] in fileExt]
+    return Files
+
 
 tk = Tk()
-tk.title("mayaExportABC")
-tk.geometry("550x150+500+500")
+tk.title('mayaExportABC')
+tk.iconbitmap('C:/aa.ico')
+tk.geometry('650x150+500+500')
 
-# Variable
-mayaVarInstPath = StringVar(tk, value=r'C:\Progra~1\Autodesk\Maya2018')
-mayaVarPath = StringVar(tk,'z:/')
-abcVarPath = StringVar(tk,'z:/')
+# 初始tkinter变量
+mayaVarInstPath = StringVar(tk, value=r'C:\Program Files\Autodesk\Maya2018')
+mayaVarPath = StringVar(tk)
+abcVarPath = StringVar(tk)
 
-# Widget
-mayaInstPathLabel = Label(tk, text="Maya Install Path")
-mayaInstPath = Entry(tk, textvariable=mayaVarInstPath)
-mayaFileLabel = Label(tk, text="Maya Path")
+# GUI界面元素
+mayaInstPathLabel = Label(tk, text='Maya Install Path', fg='gray')
+mayaInstPath = Entry(tk, textvariable=mayaVarInstPath, fg='gray')
+mayaFileLabel = Label(tk, text='Maya File or Path')
 mayaPathEntry = Entry(tk, textvariable=mayaVarPath)
-abcPathLabel = Label(tk, text="ABC Path")
+abcPathLabel = Label(tk, text='ABC Path')
 abcPathEntry = Entry(tk, textvariable=abcVarPath)
-convert = Button(tk, text="Convert", command=MayaMeltoCMD)
+noticeLabel = Label(tk, text='( Maya 和 ABC 路径目前不支持空格)',fg='green')
+convert = Button(tk, text='Convert', command = exportAllABC)
+copyright = Button(tk, text='天雷动漫 github.com/HandierChan/mayatoABC')
 
-# Layout
+# GUI界面布局
 mayaInstPathLabel.grid(row=0, sticky='e')
-mayaInstPath.grid(row=0, column='1', sticky='we',ipadx=150)
+mayaInstPath.grid(row=0, column=1, sticky='we',ipadx=60)
 mayaFileLabel.grid(row=1, sticky='e')
-mayaPathEntry.grid(row=1, column='1', sticky='we',ipadx=150)
+mayaPathEntry.grid(row=1, column=1, sticky='we',columnspan=2,ipadx=60)
 abcPathLabel.grid(row=2, sticky='e')
-abcPathEntry.grid(row=2, column='1', sticky='we',ipadx=150)
-convert.grid(row=3, column='1',sticky='w')
+abcPathEntry.grid(row=2, column=1, sticky='we',columnspan=2,ipadx=60)
+noticeLabel.grid(row=3, column=1, sticky='w')
+convert.grid(row=4, column=1,sticky='w')
+copyright.grid(row=4, column=2,sticky='e')
 
 tk.mainloop()
